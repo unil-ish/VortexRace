025febe979f9187d35c49bcf7b3ff9d24ee3bc93
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2023-04-18 17:35:59 +0200

    favorites list on the right

--- Visual/app/cook_book.py
+++ Visual/app/cook_book.py
@@ -22,42 +22,47 @@ def cook_book():
     ], default=1)
 
     if chosen_id == "1":
-        col1, col2, col3 = st.columns(3)
-        col1.header('Profil')
-        col1.checkbox('Prénom')
-        col1.checkbox('Nom')
-        col1.selectbox('Genre', ['Homme', 'Femme', 'Autres'])
-        col1.metric("Cool", "10/10")
-        age = col1.slider("Age", 16, 100)
-        with col1.expander("Lire la suite"):
-            st.write("""
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Mauris commodo augue ut dui malesuada, vel posuere neque tempus. 
-                        Vestibulum ut augue volutpat, gravida arcu in, eleifend nulla. 
-                        Praesent rhoncus tellus vel nunc auctor, non maximus dolor interdum. 
-                        Pellentesque quis vestibulum nisl. Sed blandit semper massa. 
-                        Etiam consequat urna id fermentum aliquet. 
-                        Integer mattis ligula sed nibh malesuada, at posuere magna bibendum. 
-                        Donec eu lectus eget quam luctus viverra in sed odio. 
-                        Suspendisse vehicula metus quis molestie commodo.
-                    """)
+        col1, col2, col3 = st.columns([2, 1, 2])
+        with col1:
+            col1.header('Profil')
+            col1.checkbox('Prénom')
+            col1.checkbox('Nom')
+            col1.selectbox('Genre', ['Homme', 'Femme', 'Autres'])
+            col1.metric("Cool", "10/10")
+            age = col1.slider("Age", 16, 100)
+            with col1.expander("Lire la suite"):
+                st.write("""
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Mauris commodo augue ut dui malesuada, vel posuere neque tempus. 
+                                Vestibulum ut augue volutpat, gravida arcu in, eleifend nulla. 
+                                Praesent rhoncus tellus vel nunc auctor, non maximus dolor interdum. 
+                                Pellentesque quis vestibulum nisl. Sed blandit semper massa. 
+                                Etiam consequat urna id fermentum aliquet. 
+                                Integer mattis ligula sed nibh malesuada, at posuere magna bibendum. 
+                                Donec eu lectus eget quam luctus viverra in sed odio. 
+                                Suspendisse vehicula metus quis molestie commodo.
+                            """)
+        with col3:
+            # Afficher la liste des favoris
+            favorites = mediatheque.get_favorites()
+            conn = sqlite3.connect("videos.db")
+            c = conn.cursor()
+            c.execute("SELECT * FROM favorites")
+            rows = c.fetchall()
+            st.write('### Liste des favoris:')
+            for i, row in enumerate(reversed(rows)):
+                st.write(
+                    f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+                    unsafe_allow_html=True)
+                if st.button(f"Retirer des favoris", key=f"add-to-favorites-{i}"):
+                    # Retirer la vidéo à la liste des favoris
+                    c.execute("DELETE FROM favorites WHERE video_id = ?", (row[2],))
+                    conn.commit()
+                    # Rafraîchir la page
+                    st.experimental_rerun()
 
-        # Afficher la liste des favoris
-        favorites = mediatheque.get_favorites()
-        conn = sqlite3.connect("videos.db")
-        c = conn.cursor()
-        c.execute("SELECT * FROM favorites")
-        rows = c.fetchall()
-        st.write('### Liste des favoris:')
-        for i, row in enumerate(reversed(rows)):
-            st.write(
-                f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-                unsafe_allow_html=True)
-            if st.button(f"Retirer des favoris", key=f"add-to-favorites-{i}"):
-                # Retirer la vidéo à la liste des favoris
-                c.execute("DELETE FROM favorites WHERE video_id = ?", (row[2],))
-                conn.commit()
-            st.write("")
+                st.write("")
+
 
     if chosen_id == "2":
         col1, col2, col3 = st.columns(3)
