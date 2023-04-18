4061770c7492dd778a8ab9ace19a61f19d5f2b56
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2023-04-18 16:48:02 +0200

    "add to fav" replace with a star

--- Visual/app/mediatheque.py
+++ Visual/app/mediatheque.py
@@ -46,8 +46,10 @@ def mediatheque():
     rows = c.fetchall()
     st.write('### Liste des vidéos:')
     for i, row in enumerate(reversed(rows)):
-        st.write(f' <iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', unsafe_allow_html=True)
-        if st.button(f"Add to favorites {i}"):
+        st.write(
+            f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+            unsafe_allow_html=True)
+        if st.button(f"\u2605", key=f"add-to-favorites-{i}"):
             # Ajouter la vidéo à la liste des favoris
             c.execute("INSERT INTO favorites VALUES (?, ?, ?)", (row[0], row[1], row[2]))
             conn.commit()
