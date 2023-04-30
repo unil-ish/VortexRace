7a24c2eab045117e2c6bbd072759c199ee286453
NEBULA-t <61778155+NEBULA-t@users.noreply.github.com>
2023-04-30 17:27:49 +0200

    page html et css v4

--- Visual/app/main2.py
+++ Visual/app/main2.py
@@ -2,11 +2,10 @@ import streamlit as st
 import extra_streamlit_components as stx
 import streamlit.components.v1 as components
 
-with open('./Visual/app/style3.css') as f :
+with open('./Visual/app/style4.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-HtmlFile = open("./Visual/app/index3.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read()
-#components.html(source_code)
-st.markdown(source_code,unsafe_allow_html=True)
 
+HtmlFile = open("./Visual/app/index4.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read()
+st.markdown(source_code, unsafe_allow_html=True)
 
