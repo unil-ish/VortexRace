a5c15bb729ec8e0e7fab5c8bb07e4873b6395f47
Zacatel <elijah.green.41@gmail.com>
2023-05-01 17:13:14 +0200

    finds the currently logged in username, need to know where to put this code

---
+++ Visual/app/logintests2.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import streamlit as st
+import subprocess
+from streamlit_login_auth_ui.widgets import __login__
+#import cook_book
+
+__login__obj = __login__(auth_token = "courier_auth_token",
+                    company_name = "Shims",
+                    width = 200, height = 250,
+                    logout_button_name = 'Logout', hide_menu_bool = False,
+                    hide_footer_bool = False,
+                    #lottie_url = 'https://assets8.lottiefiles.com/packages/lf20_ktwnwv5m.json'
+                    )
+__login__obj
+LOGGED_IN = __login__obj.build_login_ui()
+
+
+def get_logged_in_user():
+    if LOGGED_IN:
+        fetched_cookies = __login__obj.cookies
+        if '__streamlit_login_signup_ui_username__' in fetched_cookies.keys():
+            loggedUser = fetched_cookies['__streamlit_login_signup_ui_username__']
+            return loggedUser
+        else:
+            return "Username not found in cookies"
+    else:
+        return "User not logged in"
+
+#definit la variable
+loggedUser = get_logged_in_user()
+
+if st.session_state['LOGGED_IN'] == True:
+    st.write("Welcome, " + loggedUser + "!")
+
