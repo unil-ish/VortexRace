d5b6651d8921a704b51e3904e1ab45a353af186c
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2023-05-03 18:10:37 +0200

    fav_by and liked_by username working

--- Visual/app/main.py
+++ Visual/app/main.py
@@ -2,70 +2,88 @@ import streamlit as st
 import extra_streamlit_components as stx
 import sqlite3
 import mediatheque
+import logintests2
 
-st.set_page_config(page_title="Mes onglets", layout="wide")
+username = logintests2.get_logged_in_user()
+
+#st.set_page_config(page_title="Mes onglets", layout="wide")
 with open('Visual/app/style.css') as f :
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def main():
-    st.title('Vortex Race')
+    if username != '':
+        st.title('Vortex Race')
 
-    chosen_id = stx.tab_bar(data=[
-        stx.TabBarItemData(id=1, title="Profil", description="Mes informations"),
-        stx.TabBarItemData(id=2, title="Statistiques", description="Mes courses"),
-        stx.TabBarItemData(id=3, title="Médiathèque", description="Les vidéos"),
-    ], default=1)
+        chosen_id = stx.tab_bar(data=[
+            stx.TabBarItemData(id=1, title="Profil", description="Mes informations"),
+            stx.TabBarItemData(id=2, title="Statistiques", description="Mes courses"),
+            stx.TabBarItemData(id=3, title="Médiathèque", description="Les vidéos"),
+        ], default=1)
 
-    if chosen_id == "1":
-        col1, col2, col3 = st.columns([2, 1, 2])
-        with col1:
-            col1.header('Profil')
-            col1.checkbox('Prénom')
-            col1.checkbox('Nom')
-            col1.selectbox('Genre', ['Homme', 'Femme', 'Autres'])
-            col1.metric("Cool", "10/10")
-            age = col1.slider("Age", 16, 100)
-            with col1.expander("Lire la suite"):
-                st.write("""
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                    Mauris commodo augue ut dui malesuada, vel posuere neque tempus. 
-                                    Vestibulum ut augue volutpat, gravida arcu in, eleifend nulla. 
-                                    Praesent rhoncus tellus vel nunc auctor, non maximus dolor interdum. 
-                                    Pellentesque quis vestibulum nisl. Sed blandit semper massa. 
-                                    Etiam consequat urna id fermentum aliquet. 
-                                    Integer mattis ligula sed nibh malesuada, at posuere magna bibendum. 
-                                    Donec eu lectus eget quam luctus viverra in sed odio. 
-                                    Suspendisse vehicula metus quis molestie commodo.
-                                """)
-        with col3:
-            # Afficher la liste des favoris
-            conn = sqlite3.connect("videos.db")
-            c = conn.cursor()
-            c.execute("SELECT * FROM favorites")
-            rows = c.fetchall()
-            st.write('### Liste des favoris:')
-            for i, row in enumerate(reversed(rows)):
-                st.write(
-                    f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-                    unsafe_allow_html=True)
-                if st.button(f"Retirer des favoris", key=f"add-to-favorites-{i}"):
-                    # Retirer la vidéo à la liste des favoris
-                    c.execute("DELETE FROM favorites WHERE video_id = ?", (row[2],))
-                    conn.commit()
-                    # Rafraîchir la page
-                    st.experimental_rerun()
+        if chosen_id == "1":
+            col1, col2, col3 = st.columns([2, 1, 2])
+            with col1:
+                col1.header(username)
+                col1.checkbox('Prénom')
+                col1.checkbox('Nom')
+                col1.selectbox('Genre', ['Homme', 'Femme', 'Autres'])
+                col1.metric("Cool", "10/10")
+                age = col1.slider("Age", 16, 100)
+                with col1.expander("Lire la suite"):
+                    st.write("""
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Mauris commodo augue ut dui malesuada, vel posuere neque tempus. 
+                                        Vestibulum ut augue volutpat, gravida arcu in, eleifend nulla. 
+                                        Praesent rhoncus tellus vel nunc auctor, non maximus dolor interdum. 
+                                        Pellentesque quis vestibulum nisl. Sed blandit semper massa. 
+                                        Etiam consequat urna id fermentum aliquet. 
+                                        Integer mattis ligula sed nibh malesuada, at posuere magna bibendum. 
+                                        Donec eu lectus eget quam luctus viverra in sed odio. 
+                                        Suspendisse vehicula metus quis molestie commodo.
+                                    """)
+            with col3:
+                # Afficher la liste des favoris
+                conn = sqlite3.connect("videos.db")
+                c = conn.cursor()
+                c.execute("SELECT url, title, video_id, likes, dislikes, liked_by, disliked_by, fav_by FROM videos WHERE fav_by LIKE ?", ('%'+username+'%',))
+                rows = c.fetchall()
+                st.write('### Liste des favoris:')
+                for i, row in enumerate(reversed(rows)):
+                    # Extraire les informations de la vidéo
+                    url = row[0]
+                    title = row[1]
+                    video_id = row[2]
+                    likes = row[3]
+                    dislikes = row[4]
+                    liked_by = row[5]
+                    disliked_by = row[6]
+                    fav_by = row[7]
 
-    if chosen_id == "2":
-        col1, col2, col3 = st.columns(3)
-        col2.header('Mes statistiques')
-        val = stx.stepper_bar(steps=["Ready", "Get Set", "Go"])
+                    st.write(
+                        f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+                        unsafe_allow_html=True)
+                    if st.button(f"Retirer des favoris", key=f"add-to-favorites-{i}"):
+                        # Retirer la vidéo à la liste des favoris
+                        fav_by_list = fav_by.split(", ")
+                        fav_by_list.remove(username)
+                        fav_by = ", ".join(fav_by_list)
+                        c.execute("UPDATE videos SET fav_by = ? WHERE video_id = ?", (fav_by, video_id,))
+                        conn.commit()
+                        # Rafraîchir la page
+                        st.experimental_rerun()
 
-    if chosen_id == "3":
-        mediatheque.mediatheque()
+        if chosen_id == "2":
+            col1, col2, col3 = st.columns(3)
+            col2.header('Mes statistiques')
+            val = stx.stepper_bar(steps=["Ready", "Get Set", "Go"])
 
-        # st.text_area('Bonjour')
-        # st.text_input()
-        # st.slider()
+        if chosen_id == "3":
+            mediatheque.mediatheque()
 
+            # st.text_area('Bonjour')
+            # st.text_input()
+            # st.slider()
+    else:
+        st.warning('PLease login first')
 if __name__ == '__main__':
     main()
--- Visual/app/mediatheque.py
+++ Visual/app/mediatheque.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import sqlite3
 from pytube import YouTube
+import logintests2
+
+username = logintests2.get_logged_in_user()
+
 """
 from loginWidgetStolen import __login__
 
@@ -18,13 +22,13 @@ def mediatheque():
 
     # Créer la table pour stocker les informations de la vidéo
     c.execute('''CREATE TABLE IF NOT EXISTS videos
-                 (url text, title text, video_id text, likes integer NOT NULL DEFAULT 0, dislikes integer NOT NULL DEFAULT 0, liked_by)''')
+                 (url text, title text, video_id text, likes integer NOT NULL DEFAULT 0, dislikes integer NOT NULL DEFAULT 0, liked_by, disliked_by, fav_by)''')
     conn.commit()
 
-    # Créer la table pour stocker les vidéos favorites (doit être perso, une pour chaque user)
+    """# Créer la table pour stocker les vidéos favorites (doit être perso, une pour chaque user)
     c.execute('''CREATE TABLE IF NOT EXISTS favorites
                      (title text, author text, video_id text)''')
-    conn.commit()
+    conn.commit()"""
 
     # Demander à l'utilisateur de saisir une URL YouTube
     url = st.text_input('Entrez l\'URL de la vidéo YouTube:')
@@ -38,13 +42,16 @@ def mediatheque():
         try:
             # Extraire les informations de la vidéo YouTube
             video = YouTube(url)
-            title = video.title
+            title = ""
             video_id = video.video_id
             likes = 0
             dislikes = 0
+            liked_by = ""
+            disliked_by = ""
+            fav_by = ""
 
             # Stocker les informations de la vidéo dans la base de données
-            c.execute("INSERT INTO videos VALUES (?, ?, ?, ?, ?)", (url, title, video_id, likes, dislikes))
+            c.execute("INSERT INTO videos VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (url, title, video_id, likes, dislikes, liked_by, disliked_by, fav_by))
             conn.commit()
 
             # Afficher la vidéo dans un IFrame
@@ -55,11 +62,12 @@ def mediatheque():
             st.write(f'[Regarder sur YouTube](https://www.youtube.com/watch?v={video_id})')
 
         except Exception as e:
+            st.write(e)
             st.error('Une erreur est survenue lors de l\'extraction de la vidéo: Nous n\'acceptons que les vidéos Youtube. Il est possible que votre lien soit obsolète.')
             st.error("Si vous êtes sûr de votre lien YouTube, réessayez.")
 
     # Afficher la liste des vidéos dans la base de données
-    c.execute("SELECT url, title, video_id, likes, dislikes, (likes - dislikes) AS difference FROM videos ORDER BY difference DESC ")
+    c.execute("SELECT url, title, video_id, likes, dislikes, liked_by, disliked_by, fav_by, (likes - dislikes) AS difference FROM videos ORDER BY difference DESC ")
     rows = c.fetchall()
     st.write('### Liste des vidéos:')
 
@@ -71,7 +79,9 @@ def mediatheque():
         video_id = row[2]
         likes = row[3]
         dislikes = row[4]
-        is_liked = row[5]
+        liked_by = row[5]
+        disliked_by = row[6]
+        fav_by = row[7]
 
         st.write(
             f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{row[2]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
@@ -81,37 +91,77 @@ def mediatheque():
 
         with col_fav:
             if st.button(f"\u2605", key=f"add-to-favorites-{i}"):
-                # Ajouter la vidéo à la liste des favoris
-                c.execute("INSERT INTO favorites VALUES (?, ?, ?)", (row[0], row[1], row[2]))
-                conn.commit()
-                st.write("**La vidéo a bien été ajoutée à vos favoris!**", unsafe_allow_html=True)
+                # Ajouter la vidéo à la liste des favoris (ajout du username à fav_by)
+                fav_by_list = fav_by.split(", ")
+                if username not in fav_by_list:
+                    fav_by_list.append(username)
+                    fav_by = ", ".join(fav_by_list)
+                    c.execute("UPDATE videos SET fav_by = ? WHERE video_id = ?", (fav_by, video_id,))
+                    conn.commit()
+                    st.write("**La vidéo a bien été ajoutée à vos favoris!**", unsafe_allow_html=True)
+                else:
+                    fav_by_list.remove(username)
+                    fav_by = ", ".join(fav_by_list)
+                    c.execute("UPDATE videos SET fav_by = ? WHERE video_id = ?", (fav_by, video_id,))
+                    conn.commit()
+                    st.write("**La vidéo a été retirée de vos favoris!**", unsafe_allow_html=True)
 
         with col_like:
-            c.execute("SELECT likes FROM videos WHERE video_id = ?", (video_id,))
+            c.execute("SELECT likes, dislikes, liked_by, disliked_by FROM videos WHERE video_id = ?", (video_id,))
             result = c.fetchone()
             if result is not None:
                 likes = result[0]
                 st.write(f'Likes : {likes}')
+                st.write(f'Liked by : {liked_by}')
             if st.button(f"\U0001F44D", key=f"like-{i}"):
                 # Ajouter un like à la vidéo et le username à liked_by
-                """
-                c.execute("UPDATE videos SET likes = likes + 1, liked_by = ? WHERE video_id = ?", (username, video_id,))
-                conn.commit()"""
-                # Ajouter un like (en attendant que le comm d'au dessus marche)
-                c.execute("UPDATE videos SET likes = likes + 1 WHERE video_id = ?", (video_id,))
-                conn.commit()
+                liked_by_list = liked_by.split(", ")
+                disliked_by_list = disliked_by.split(", ")
+                if username not in liked_by_list:
+                    liked_by_list.append(username)
+                    liked_by = ", ".join(liked_by_list)
+                    c.execute("UPDATE videos SET likes = likes + 1, liked_by = ? WHERE video_id = ?", (liked_by, video_id,))
+                    conn.commit()
+                    if username in disliked_by_list:
+                        disliked_by_list.remove(username)
+                        disliked_by = ", ".join(disliked_by_list)
+                        c.execute("UPDATE videos SET dislikes = dislikes - 1, disliked_by = ? WHERE video_id = ?",
+                                  (disliked_by, video_id,))
+                        conn.commit()
+                else:
+                    liked_by_list.remove(username)
+                    liked_by = ", ".join(liked_by_list)
+                    c.execute("UPDATE videos SET likes = likes - 1, liked_by = ? WHERE video_id = ?", (liked_by, video_id,))
+                    conn.commit()
                 st.experimental_rerun()
 
         with col_dislike:
-            c.execute("SELECT dislikes FROM videos WHERE video_id = ?", (video_id,))
+            c.execute("SELECT likes, dislikes, liked_by, disliked_by FROM videos WHERE video_id = ?", (video_id,))
             result = c.fetchone()
             if result is not None:
-                dislikes = result[0]
+                dislikes = result[1]
                 st.write(f'Dislikes : {dislikes}')
+                st.write(f'Disliked by : {disliked_by}')
             if st.button(f"\U0001F44E", key=f"dislike-{i}"):
                 # Ajouter un dislike à la vidéo
-                c.execute("UPDATE videos SET dislikes = dislikes + 1 WHERE video_id = ?", (video_id,))
-                conn.commit()
+                liked_by_list = liked_by.split(", ")
+                disliked_by_list = disliked_by.split(", ")
+                if username not in disliked_by_list:
+                    disliked_by_list.append(username)
+                    disliked_by = ", ".join(disliked_by_list)
+                    c.execute("UPDATE videos SET dislikes = dislikes + 1, disliked_by = ? WHERE video_id = ?", (disliked_by, video_id,))
+                    conn.commit()
+                    if username in liked_by_list:
+                        liked_by_list.remove(username)
+                        liked_by = ", ".join(liked_by_list)
+                        c.execute("UPDATE videos SET likes = likes - 1, liked_by = ? WHERE video_id = ?",
+                                  (liked_by, video_id,))
+                        conn.commit()
+                else:
+                    disliked_by_list.remove(username)
+                    disliked_by = ", ".join(disliked_by_list)
+                    c.execute("UPDATE videos SET dislikes = dislikes - 1, disliked_by = ? WHERE video_id = ?", (disliked_by, video_id,))
+                    conn.commit()
                 st.experimental_rerun()
 
         with col4:
@@ -122,4 +172,3 @@ def mediatheque():
                 # Rafraîchir la page
                 st.experimental_rerun()
 
-mediatheque()
